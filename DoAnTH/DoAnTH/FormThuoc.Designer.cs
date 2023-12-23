015d22344bc77723c90904eb988194096021af25
nguyenngockhoa23 <kenkonoob23@gmail.com>
2023-12-23 09:52:27 +0700

    formThuoc

---
+++ DoAnTH/DoAnTH/FormThuoc.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace DoAnTH
+{
+    partial class FormThuoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnX = new System.Windows.Forms.Button();
+            this.buttonTim = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbLoaiThuoc = new System.Windows.Forms.ComboBox();
+            this.dgvThuoc = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtCachDung = new System.Windows.Forms.TextBox();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.txtLieuDung = new System.Windows.Forms.TextBox();
+            this.txtTacDung = new System.Windows.Forms.TextBox();
+            this.txtTenThuoc = new System.Windows.Forms.TextBox();
+            this.txtMaThuoc = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThuoc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnX
+            // 
+            this.btnX.Location = new System.Drawing.Point(573, 259);
+            this.btnX.Margin = new System.Windows.Forms.Padding(2);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(20, 21);
+            this.btnX.TabIndex = 61;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // buttonTim
+            // 
+            this.buttonTim.Location = new System.Drawing.Point(447, 259);
+            this.buttonTim.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTim.Name = "buttonTim";
+            this.buttonTim.Size = new System.Drawing.Size(42, 21);
+            this.buttonTim.TabIndex = 60;
+            this.buttonTim.Text = "Tìm";
+            this.buttonTim.UseVisualStyleBackColor = true;
+            this.buttonTim.TextChanged += new System.EventHandler(this.buttonTim_TextChanged);
+            this.buttonTim.Click += new System.EventHandler(this.buttonTim_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(94, 260);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 17);
+            this.label7.TabIndex = 59;
+            this.label7.Text = "Loại thuốc";
+            // 
+            // cmbLoaiThuoc
+            // 
+            this.cmbLoaiThuoc.FormattingEnabled = true;
+            this.cmbLoaiThuoc.Location = new System.Drawing.Point(164, 256);
+            this.cmbLoaiThuoc.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbLoaiThuoc.Name = "cmbLoaiThuoc";
+            this.cmbLoaiThuoc.Size = new System.Drawing.Size(258, 21);
+            this.cmbLoaiThuoc.TabIndex = 58;
+            // 
+            // dgvThuoc
+            // 
+            this.dgvThuoc.AllowUserToAddRows = false;
+            this.dgvThuoc.AllowUserToDeleteRows = false;
+            this.dgvThuoc.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvThuoc.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvThuoc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThuoc.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgvThuoc.Location = new System.Drawing.Point(77, 289);
+            this.dgvThuoc.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvThuoc.Name = "dgvThuoc";
+            this.dgvThuoc.ReadOnly = true;
+            this.dgvThuoc.RowHeadersWidth = 51;
+            this.dgvThuoc.RowTemplate.Height = 24;
+            this.dgvThuoc.Size = new System.Drawing.Size(556, 148);
+            this.dgvThuoc.TabIndex = 57;
+            this.dgvThuoc.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThuoc_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column1.DataPropertyName = "MaThuoc";
+            this.Column1.HeaderText = "Mã thuốc";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "TenThuoc";
+            this.Column2.HeaderText = "Tên thuốc";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "TacDung";
+            this.Column3.HeaderText = "Tác dụng";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "LieuDung";
+            this.Column4.HeaderText = "Liều dùng";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "CachDung";
+            this.Column5.HeaderText = "Cách dùng";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "LoaiThuoc";
+            this.Column6.HeaderText = "Loại thuốc";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(449, 145);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(51, 21);
+            this.btnSua.TabIndex = 54;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(449, 174);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(51, 21);
+            this.btnXoa.TabIndex = 55;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(449, 117);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(51, 21);
+            this.btnThem.TabIndex = 53;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtCachDung
+            // 
+            this.txtCachDung.Location = new System.Drawing.Point(164, 228);
+            this.txtCachDung.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCachDung.Name = "txtCachDung";
+            this.txtCachDung.Size = new System.Drawing.Size(258, 20);
+            this.txtCachDung.TabIndex = 52;
+            // 
+            // txtTim
+            // 
+            this.txtTim.Location = new System.Drawing.Point(503, 260);
+            this.txtTim.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(67, 20);
+            this.txtTim.TabIndex = 56;
+            // 
+            // txtLieuDung
+            // 
+            this.txtLieuDung.Location = new System.Drawing.Point(164, 201);
+            this.txtLieuDung.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLieuDung.Name = "txtLieuDung";
+            this.txtLieuDung.Size = new System.Drawing.Size(258, 20);
+            this.txtLieuDung.TabIndex = 51;
+            // 
+            // txtTacDung
+            // 
+            this.txtTacDung.Location = new System.Drawing.Point(164, 173);
+            this.txtTacDung.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTacDung.Name = "txtTacDung";
+            this.txtTacDung.Size = new System.Drawing.Size(258, 20);
+            this.txtTacDung.TabIndex = 50;
+            // 
+            // txtTenThuoc
+            // 
+            this.txtTenThuoc.Location = new System.Drawing.Point(164, 145);
+            this.txtTenThuoc.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenThuoc.Name = "txtTenThuoc";
+            this.txtTenThuoc.Size = new System.Drawing.Size(258, 20);
+            this.txtTenThuoc.TabIndex = 49;
+            // 
+            // txtMaThuoc
+            // 
+            this.txtMaThuoc.Location = new System.Drawing.Point(164, 117);
+            this.txtMaThuoc.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaThuoc.Name = "txtMaThuoc";
+            this.txtMaThuoc.Size = new System.Drawing.Size(258, 20);
+            this.txtMaThuoc.TabIndex = 48;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(159, 63);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(369, 37);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Quản lý thông tin thuốc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(94, 232);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Cách dùng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(94, 203);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Liều dùng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(94, 175);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Tác dụng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(94, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Tên thuốc";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(94, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Mã thuốc";
+            // 
+            // FormThuoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 499);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.buttonTim);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbLoaiThuoc);
+            this.Controls.Add(this.dgvThuoc);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtCachDung);
+            this.Controls.Add(this.txtTim);
+            this.Controls.Add(this.txtLieuDung);
+            this.Controls.Add(this.txtTacDung);
+            this.Controls.Add(this.txtTenThuoc);
+            this.Controls.Add(this.txtMaThuoc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormThuoc";
+            this.Text = "FormThuoc";
+            this.Load += new System.EventHandler(this.FormThuoc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThuoc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button buttonTim;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbLoaiThuoc;
+        private System.Windows.Forms.DataGridView dgvThuoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtCachDung;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.TextBox txtLieuDung;
+        private System.Windows.Forms.TextBox txtTacDung;
+        private System.Windows.Forms.TextBox txtTenThuoc;
+        private System.Windows.Forms.TextBox txtMaThuoc;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DoAnTH/DoAnTH/FormThuoc.cs
@@ -0,0 +1,190 @@
+﻿using DoAnTH.CXL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnTH
+{
+    public partial class FormThuoc : Form
+    {
+        readonly CXulyT xulyT = new CXulyT();
+        CThuoc T = new CThuoc();
+        ErrorProvider erp = new ErrorProvider();
+        readonly CXuLyLT xulyLT = new CXuLyLT();
+        public FormThuoc()
+        {
+            InitializeComponent();
+            HienThi();
+            htcmb();
+        }
+        void clear()
+        {
+            txtMaThuoc.Text = " ";
+            txtTenThuoc.Text = " ";
+            txtTacDung.Text = " ";
+            txtLieuDung.Text = " ";
+            txtCachDung.Text = " ";
+        }
+        private void HienThi()
+        {
+            dgvThuoc.DataSource = xulyT.getALL().ToList();
+            clear();
+            dgvThuoc.ClearSelection();
+        }
+        private bool IsValidData()
+        {
+            erp.SetError(txtMaThuoc, "");
+            erp.SetError(txtTenThuoc, "");
+            erp.SetError(txtTacDung, "");
+            erp.SetError(txtLieuDung, "");
+            erp.SetError(txtCachDung, "");
+            bool problem = false;
+            if (string.IsNullOrWhiteSpace(txtMaThuoc.Text))
+            {
+                erp.SetError(txtMaThuoc, "Không để trống!");
+                problem = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtTenThuoc.Text))
+            {
+                erp.SetError(txtTenThuoc, "Không để trống!");
+                problem = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtTacDung.Text))
+            {
+                erp.SetError(txtTacDung, "Không để trống!");
+                problem = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtLieuDung.Text))
+            {
+                erp.SetError(txtLieuDung, "Không để trống!");
+                problem = true;
+            }
+            if (string.IsNullOrWhiteSpace(txtCachDung.Text))
+            {
+                erp.SetError(txtCachDung, "Không để trống!");
+                problem = true;
+            }
+            return !problem;
+        }
+        private CThuoc getT()
+        {
+            CThuoc t = new CThuoc();
+            t.MaThuoc = txtMaThuoc.Text;
+            t.TenThuoc = txtTenThuoc.Text;
+            t.CachDung = txtCachDung.Text;
+            t.LieuDung = txtLieuDung.Text;
+            t.TacDung = txtTacDung.Text;
+            string maLoaiThuoc = (string)cmbLoaiThuoc.SelectedValue;
+            t.LoaiThuoc = xulyLT.getMa(maLoaiThuoc);
+            return t;
+        }
+        private void htcmb()
+        {
+
+            cmbLoaiThuoc.DataSource = xulyLT.getALL();
+            cmbLoaiThuoc.ValueMember = "MaLoaiThuoc";
+            cmbLoaiThuoc.DisplayMember = "TenLoaiThuoc";
+        }
+
+        private void FormThuoc_Load(object sender, EventArgs e)
+        {
+            htcmb();
+            dgvThuoc.AutoGenerateColumns = false;
+            HienThi();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                CThuoc lt = getT();
+                xulyT.them(lt);
+                HienThi();
+            }
+        }
+
+        private void dgvThuoc_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                T = (CThuoc)dgvThuoc.Rows[e.RowIndex].DataBoundItem;
+                txtMaThuoc.Text = T.MaThuoc.ToString();
+                txtTenThuoc.Text = T.TenThuoc.ToString();
+                txtTacDung.Text = T.TacDung.ToString();
+                txtLieuDung.Text = T.LieuDung.ToString();
+                txtCachDung.Text = T.CachDung.ToString();
+                cmbLoaiThuoc.SelectedItem = (T.LoaiThuoc);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                CThuoc newt = getT();
+                if (!xulyT.sua(newt))
+                {
+                    MessageBox.Show("Sửa thất bại!");
+                }
+                HienThi();
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (T == null)
+            {
+                MessageBox.Show(@"Chọn thông tin cần xóa ", @"Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!xulyT.xoa(T))
+            {
+                MessageBox.Show("Xóa thất bại!");
+            }
+            HienThi();
+        }
+
+        private void buttonTim_Click(object sender, EventArgs e)
+        {
+            string Ma = txtTim.Text;
+            CThuoc t = xulyT.getMa(Ma);
+            if (t != null)
+            {
+                BindingSource bs = new BindingSource();
+                bs.DataSource = t;
+                dgvThuoc.DataSource = bs;
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy!");
+            }
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            dgvThuoc.DataSource = xulyT.getALL().ToList();
+        }
+
+        private void buttonTim_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTim.Text))
+            {
+                string Ma = txtTim.Text;
+                List<CThuoc> bn = xulyT.getLike(Ma);
+                BindingSource bs = new BindingSource();
+                bs.DataSource = bn;
+                dgvThuoc.DataSource = bs;
+            }
+            else
+            {
+                HienThi();
+            }
+        }
+    }
+}
